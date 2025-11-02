32b6da442c123490f3dd6524224d00d594c259e2
kabir5145 <kabir10112006@gmail.com>
2025-11-03 00:07:32 +0530

    reversing an array

--- src/com/company/Array_2.java
+++ src/com/company/Array_2.java
@@ -1,6 +1,8 @@
 package com.company;
 
 public class Array_2 {
+    /*
+}
     public void printArray(int[]arr){
         int n = arr.length;
         for(int i = 0;i<n;i++){
@@ -8,9 +10,24 @@ public class Array_2 {
         }
         System.out.println();
     }
-    public static void main(String[] args) {
-        Array_2 a = new Array_2();
-        int []arr = {12,3,5,4,6,11};
-        a.printArray(arr);
+ */
+        public void reverseArray(int[] arr, int start, int end) {
+            while (start < end) {
+                int temp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = temp;
+                start++;
+                end--;
+            }
+        }
+        public static void main(String[] args) {
+            int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+            Array_2 a = new Array_2();
+            a.reverseArray(arr,0,arr.length-1);
+            System.out.println("The reversed array is : ");
+            for(int x : arr){
+                System.out.print(x+" ");
+            }
+        }
     }
-}
+
