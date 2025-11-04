3c1249e2b1783562283e44497be34e95aad4b33d
kabir5145 <kabir10112006@gmail.com>
2025-11-04 15:30:44 +0530

    Maximum and minimum number in an array

--- src/com/company/Array_2.java
+++ src/com/company/Array_2.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Array_2 {
     /*
 }
@@ -11,6 +13,7 @@ public class Array_2 {
         System.out.println();
     }
  */
+    /*
         public void reverseArray(int[] arr, int start, int end) {
             while (start < end) {
                 int temp = arr[start];
@@ -20,14 +23,37 @@ public class Array_2 {
                 end--;
             }
         }
+     */
+
+    public void maxAndMin(int []arr){
+        int max = arr[0];
+        int min=arr[0];
+        for(int i = 0;i<arr.length;i++){
+            if(max<arr[i]){
+                max=arr[i];
+            } else if (min>arr[i]) {
+                min=arr[i];
+            }
+        }
+        //Print the array
+        for(int x:arr){
+            System.out.print(x+" ");
+        }
+        System.out.println();
+            System.out.println("The maximum number in this array is : "+max);
+            System.out.println("The minimum number in this array is : "+min);
+    }
         public static void main(String[] args) {
             int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
             Array_2 a = new Array_2();
+            /*
             a.reverseArray(arr,0,arr.length-1);
             System.out.println("The reversed array is : ");
             for(int x : arr){
                 System.out.print(x+" ");
             }
+             */
+            a.maxAndMin(arr);
         }
     }
 
